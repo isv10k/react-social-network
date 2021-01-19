66fe43e8c0e23a7c7c8257756b2557caea5477e4
Lo7u5 <cyymrak@gmail.com>
2021-01-19 13:55:08 +0500

    12

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <header>
+        <img src="./../logo192.png" />
+      </header>
+    );
+}
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+const Profile = () => {
+    return (
+        <div className='content'>
+        <div>
+          <img src='https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse3.mm.bing.net%2Fth%3Fid%3DOIP.Cp2S7t-jug_7o-eWKkCMXwHaEO%26pid%3DApi&f=1' />
+        </div>
+        <div>
+          <img className='ava' src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.ZNF3UzwEu_aKaw6Jh8wdrwHaNK%26pid%3DApi&f=1" />
+        </div>
+        <div>
+          my posts
+          <div>
+            new post
+          </div>
+          <div>
+            post1
+          </div>
+          <div>
+            post2
+          </div>
+        </div>
+      </div>
+    );
+}
+
+export default Profile;
