1b62e306d4950b250aee69191143a4e53ffea25d
Lo7u5 <cyymrak@gmail.com>
2021-01-19 11:37:58 +0500

    '123'

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import './App.css';
-import Header from './Header';
-import Technologies from './Technologies';
+
 
 const App = () => {
   return (
     <div>
-      <Header />
-      <Technologies />
+      
     </div>
   );
 }
