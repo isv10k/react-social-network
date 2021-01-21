70f37b54f859bded4280e543aaa8ba68c3599ee2
Lo7u5 <cyymrak@gmail.com>
2021-01-21 15:49:42 +0500

    improve front

--- src/components/Profile/MyPosts/Post/Post.jsx
+++ src/components/Profile/MyPosts/Post/Post.jsx
@@ -6,8 +6,12 @@ const Post = (props) => {
         <div className={s.item}>
             <img src='https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.popjustice.com%2Fwp-content%2Fuploads%2F2020%2F09%2FAva-Max-2020-press.jpg&f=1&nofb=1' alt='ava' />
             {props.message}
-            {props.likeCount}
-            <span>Like</span>
+            <div>
+                {props.likeCount}
+            </div>
+            <span>
+                Like
+            </span>
         </div>
     );
 }
