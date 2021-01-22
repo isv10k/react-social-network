eb0a39d4439244e3de05ed7d8fbccf00a8ac9156
Lo7u5 <cyymrak@gmail.com>
2021-01-22 15:58:15 +0500

    add Messages component

---
+++ src/components/Messages/Messages.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import s from './Messages.module.css';
+
+const Messages = (props) => {
+    return (
+        <div>
+            <span>Dialogs</span>
+        </div>
+    )
+}
+
+export default Messages;
